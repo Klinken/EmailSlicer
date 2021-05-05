1741cee0bc77a721049328ac6a5584f8c901287e
Soren <klinkwort92@gmail.com>
2021-05-05 10:09:55 +0200

    Created method to ask for email
    This method ask the user for their email and uses a regex to check if it is a correct formatted email address.

--- EmailSlicer/Program.cs
+++ EmailSlicer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace EmailSlicer
 {
@@ -8,11 +9,40 @@ namespace EmailSlicer
         {
             // Variables
 
+            string usersEmail;
+
             // Methods
+
+                // Ask for users email and store it
+
+            string askForEmail()
+            {
+                // Make sure its an email
+                Regex regex = new Regex(@"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?");
+
+                string email = Console.ReadLine();
+
+
+
+                if (!regex.IsMatch(email))
+                {
+                    Console.WriteLine("There seems to be a problem with your email, please try again.");
+
+                    return askForEmail();
+
+                }
+
+                return usersEmail = email;
+
+            }
+
+            // Check users email for custom or popular domain
 
             // Execution
 
+            askForEmail();
 
+            Console.WriteLine($"{usersEmail}");
         }
     }
 }
